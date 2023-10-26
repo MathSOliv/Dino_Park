4e47b7e9d210c949e956dfe379e92db50183200a
MathSOliv <matheus.silvagab@gmail.com>
2023-10-26 17:28:17 -0300

    testing new features

--- src/componentes/DinoBanner/index.js
+++ src/componentes/DinoBanner/index.js
@@ -4,7 +4,9 @@ const DinoBanner = () => {
     return(
         <div className='dinoDiv'>
             <img src='/imagens/skeleton_t-rex.jpg' alt='dinossauro banner'/>
-            <p>O maior parque</p>
+            <div className='banner_text'>
+                <div className='text'><p>Um Mundo Perdido de Maravilhas Pré-Históricas</p></div>
+            </div>
         </div>
     )
 }
